7ea29c6f9da4a59232cd0cfedf95d95ad4cde80b
DAMILARE69 <104268165+DAMILARE69@users.noreply.github.com>
2022-05-13 09:25:13 -0700

    used as={motion.div} from framer motion

--- src/Components/PageWarpper/PageWarpper.js
+++ src/Components/PageWarpper/PageWarpper.js
@@ -4,10 +4,11 @@ import { PageWarpperComponent } from "./PageWarpperStyles";
 export function PageWarpper({ children }) {
   return (
     <PageWarpperComponent
-      exit={{ y: "-100vh", duration: 0.5, transition: { ease: "easeInOut" } }}
+      as={motion.section}
+      exit={{ x: "-100vw", duration: 0.5, transition: { ease: "easeInOut" } }}
       transition={{ duration: 0.5 }}
-      initial={{ y: 300, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
+      initial={{ x: 300, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
     >
       {children}
     </PageWarpperComponent>
