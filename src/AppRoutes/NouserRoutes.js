28c48dca6a839fdbbc0fccbf66f39b028b2aa952
DAMILARE69 <104268165+DAMILARE69@users.noreply.github.com>
2022-05-14 07:21:49 -0700

    ftgj

--- src/AppRoutes/NouserRoutes.js
+++ src/AppRoutes/NouserRoutes.js
@@ -2,13 +2,16 @@
 //NouserRoutes
 
 import { Outlet } from "react-router-dom";
+import { PageWarpper } from "../Components/PageWarpper/PageWarpper";
 import { useAuth } from "../Hooks/useAuth/useAuth";
 export function NouserRoutes() {
   const { user, signout } = useAuth();
   return user ? (
-    <h1>
-      Go to Dashboard <button onClick={() => signout()}>log out</button>
-    </h1>
+    <PageWarpper>
+      <h1>
+        Go to Dashboard <button onClick={() => signout()}>log out</button>
+      </h1>
+    </PageWarpper>
   ) : (
     <Outlet />
   );
